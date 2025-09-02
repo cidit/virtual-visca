77d4c77233ba9611cf9044722abf0a50c688d812
cidit <44065673+cidit@users.noreply.github.com>
2025-09-02 14:38:35 +0000

    added clap, started impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap;
+use std::net::SocketAddr;
+use bevy::prelude as bv;
+
+
+#[derive(clap::Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+    #[arg(short, long, value_name = "ADDRESS:PORT")]
+    visca_bind: SocketAddr,
+
+
+    #[arg(short, long, value_name = "ADDRESS:PORT")]
+    video_bind: SocketAddr,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if cli.debug > 0 {
+        println!("Hello, virtual-visca!");
+    }
+
+    bv::App::new().run();
 }
